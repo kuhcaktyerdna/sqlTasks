9cb6a2b9c5845d8a3317955c729a2ae9ddd5f20f
Andrey Tkachuk <32371476+kuhcaktyerdna@users.noreply.github.com>
2018-05-10 17:54:40 +0300

    used both variants with regexp_like and without it

--- part1/14.sql
+++ part1/14.sql
@@ -1,4 +1,8 @@
 --14. Напишите запрос, который может вывести всех заказчиков, чьи имена начинаются с буквы попадающей в диапазон от A до G.
 select cname 
 from customers 
+where regexp_like(cname, '^[A-G]');
+
+select cname 
+from customers 
 where cname between 'A' and 'H';
