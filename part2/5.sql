cda0804015c6dc14d5f243be9edcb1506ddf0807
kuhcaktyerdna <kuhcaktyerdna@gmail.com>
2018-05-18 17:15:37 +0300

    small fix

--- part2/5.sql
+++ part2/5.sql
@@ -1,12 +1,13 @@
---5. Напишите запрос, использующий ANY или ALL, который бы находил всех продавцов, которые не имеют никаких заказчиков, размещенных в их городе.
-select * 
-from salespeople 
+--5. Напишите запрос, использующий ANY или ALL, который бы находил всех продавцов, которые не имеют никаких заказчиков,
+-- размещенных в их городе.
+select *
+from salespeople
 where city != all
 (select city
 from customers);
 
-select * 
-from salespeople 
-where not city = any
+select *
+from salespeople
+where not (city = any
 (select city
-from customers);
+from customers));
